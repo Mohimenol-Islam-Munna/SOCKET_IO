4dfb2fafd4ed83f495e91e6637206794445a2604
Mohimenol-Islam-Munna <munna.cse.pust@gmail.com>
2022-06-09 15:27:22 +0600

    basic group chat application

---
+++ basic_group_chat/server/broadcastServer.js
@@ -0,0 +1,44 @@
+const http = require("http");
+const path = require("path");
+const express = require("express");
+const { Server } = require("socket.io");
+
+const app = express();
+const httpServer = http.createServer(app);
+
+const io = new Server(httpServer, {});
+
+// routes
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "client", "broadcastClient.html"));
+});
+
+// default  connection for all
+io.on("connection", (socket) => {
+  setTimeout(() => {
+    io.sockets.emit("firtBroadcast", "This data is broadcasting for you!");
+  }, 6000);
+
+  //   disconnect user
+  socket.on("disconnect", () => {
+    console.log("user disconnected ::", socket.id);
+  });
+});
+
+// Namespace :: player
+let playerNamespace = io.of("/player");
+playerNamespace.on("connection", (socket) => {
+  playerNamespace.emit("player", "Data for player namespace");
+});
+
+// Namespace :: cusotmer
+let customerNamespace = io.of("/customer");
+customerNamespace.on("connection", (socket) => {
+  customerNamespace.emit("customer", "data for customer namespace");
+});
+
+// listen server
+const PORT = "3006";
+httpServer.listen(PORT, () => {
+  console.log(`Your Broadcast server is runnig at PORT ${PORT}...`);
+});
---
+++ basic_group_chat/server/server.js
@@ -0,0 +1,20 @@
+const http = require("http");
+const path = require("path");
+const express = require("express");
+const { Server } = require("socket.io");
+
+const app = express();
+const httpServer = http.createServer(app);
+
+const io = new Server(httpServer, {});
+
+// routes
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "client", "index.html"));
+});
+
+// listen server
+const PORT = "3005";
+httpServer.listen(PORT, () => {
+  console.log(`Your Server is runnig at PORT ${PORT}...`);
+});
